0cac108956abb156f88a8a3993748df273c40f94
reneeb <info@perl-services.de>
2018-01-02 10:50:24 +0100

    show button to open search result in new window

--- Custom/Kernel/Output/HTML/OutputFilterToolbarTicketNumberSearch.pm
+++ Custom/Kernel/Output/HTML/OutputFilterToolbarTicketNumberSearch.pm
@@ -47,17 +47,28 @@ sub Run {
 
     my $LayoutObject   = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
     my $LanguageObject = $Kernel::OM->Get('Kernel::Language');
+    my $ConfigObject   = $Kernel::OM->Get('Kernel::Config');
 
     my $Baselink    = $LayoutObject->{Baselink};
     my $Description = $LanguageObject->Translate("TicketNumber") || 'TicketNumber';
 
+    my $Button = '';
+    if ( $ConfigObject->Get('ToolbarTicketNumberSearch::ShowButtonToOpenNewWindow') ) {
+        $Button = q~
+            <button onclick="$('#SearchTicketNumber').attr('target','_blank');">
+                <span class="fa fa-external-link"></span>
+            </button>
+        ~;
+    }
+
     my $Form = qq~
         <li class="Extended SearchFulltext" style="margin-left: 10px">
-            <form action="$Baselink" method="post" name="SearchTicketNumber">
+            <form action="$Baselink" method="post" name="SearchTicketNumber" id="SearchTicketNumber">
                 <input type="hidden" name="Action" value="AgentTicketSearch"/>
                 <input type="hidden" name="Subaction" value="Search"/>
                 <input type="hidden" name="CheckTicketNumberAndRedirect" value="1"/>
                 <input type="text" size="25" name="TicketNumber" id="TicketNumber" placeholder="$Description" title="$Description"/>
+                $Button
             </form>
         </li>
     ~;
